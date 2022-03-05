8965423e0fd88491221152628ca4678efaea626c
Rana.ismael <rana4ismael@gmail.com>
2022-03-05 08:35:11 -0500

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './user/login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoutComponent } from './user/logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../user/auth.service';
+import { User } from '../user/user.modle';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+  isLoggedIn(): boolean{
+    return this.authService.loggedIn;
+  }
+  getRole(): string{
+    let data: User = this.authService.retrieveUser();
+    return data.role;
+
+  }
+}
