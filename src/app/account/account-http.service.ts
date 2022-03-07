08f0ce673c5e3856e660d4f5886c3cce69864884
Rana.ismael <rana4ismael@gmail.com>
2022-03-07 02:47:21 -0500

    second

--- src/app/account/account-http.service.ts
+++ src/app/account/account-http.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {Account} from './account.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountHttpService {
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) { }
+
+  fetchAllAccount():Observable<Account[]> {
+    return this.http.get<Account[]>("http://localhost:4040/api/books");
+   
+  }
+  deleteAccount(userId: number): Observable<Account[]> {
+    return this.http.delete<Account[]>("http://localhost:4040/api/books/"+userId);
+      }
+      addAccount(accountModel: Account) :Observable<Account> {
+        return this.http.post<Account>("http://localhost:4040/api/books", JSON.stringify(accountModel));
+  }
+  updateAccount(accountModel: Account): Observable<Account> {
+    return this.http.put<Account>("http://localhost:4040/api/books", JSON.stringify(accountModel));
+      }
+    
+      fetchAAccount(userId: number):Observable<Account> {
+        return this.http.get<Account>("http://localhost:4040/api/books/"+userId);
+        
+    
+      }
+  
+      }
+  
+  
+
+
+
--- src/app/account/list-http-account/list-http-account.component.ts
+++ src/app/account/list-http-account/list-http-account.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountHttpService } from '../account-http.service';
+import { Account } from '../account.model';
 
 @Component({
   selector: 'app-list-http-account',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListHttpAccountComponent implements OnInit {
 
-  constructor() { }
+  allAccount: Account[] = [];
+  toggleAdd: boolean = false;
 
-  ngOnInit(): void {
+  newAccount: Account = {
+    userId: 0,
+    username: '',
+    password: '',
+    fullName: '',
+    email: '',
+    roleId: 0,
+    role: ''
   }
 
+  constructor(private accountHttpService: AccountHttpService,private router: Router) { }
+
+  ngOnInit(): void {
+this.loadAllAccount
+  }
+  
+  toggleAddForm() {
+    if(this.toggleAdd) {
+      this.toggleAdd = false;
+    } else {
+      this.toggleAdd = true;
+    }
+  }
+  loadAllAccount(){
+    this.accountHttpService.fetchAllAccount().subscribe((response)=>{
+      // the response that we receive here is an array of Book
+      console.log(response);
+      this.allAccount = response;
+    });
+
+  }
+    
+  deleteAccount(userId: number) {
+     this.accountHttpService.deleteAccount(userId).subscribe((response)=>{
+      console.log(response);
+     
+      this.loadAllAccount();
+  })
+  
 }
+  
+}
+
